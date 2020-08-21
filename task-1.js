996c104904ed418c99c1cb2ac2cba722cff95d0a
abakarovmaks <abakarovmaks@gmail.com>
2020-08-21 12:52:51 +0300

    Решает задачу 1

---
+++ task-1.js
@@ -0,0 +1,16 @@
+const logItems = function (array) {
+  //   "use strict";
+  let number = 0;
+  let result = "";
+
+  for (let i = 0; i < array.length; i += 1) {
+    number = i + 1;
+    result += `${number} - ${array[i]}\n`;
+  }
+
+  return result;
+};
+
+console.log(logItems(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]));
+
+console.log(logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]));
