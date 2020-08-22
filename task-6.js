662ed8d6a1c99a037db902c6bbc41fef0633b371
abakarovmaks <abakarovmaks@gmail.com>
2020-08-22 20:13:38 +0300

    Решает задачу 6

---
+++ task-6.js
@@ -0,0 +1,12 @@
+function mapArray(array) {
+  "use strict";
+  const numbers = new Array(array.length);
+  for (let i = 0; i < array.length; i += 1) {
+    numbers[i] = array[i] * 10;
+  }
+  return numbers;
+}
+
+console.log(mapArray([-2, 0, 2]));
+
+console.log(mapArray([-2.5, 0, 2.5]));
